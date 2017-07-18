f3f6de5ee6c248abed3dda92be10c237d2a61299
Evan Turner <evturn@gmail.com>
2017-07-17 21:56:02 -0400

    Add second recursive function exercise

--- 06-recursive-functions.hs
+++ 06-recursive-functions.hs
@@ -4,3 +4,8 @@
 factorial :: Int -> Int
 factorial 0         = 1
 factorial n | n > 0 = n * factorial (n - 1)
+
+-- 2.
+sumdown :: Int -> Int
+sumdown 0         = 0
+sumdown n | n > 0 = n + sumdown (n - 1)
