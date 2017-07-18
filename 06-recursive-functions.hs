9459356a68b42fbbb45ab2aa14399fb08ad5fc1d
Evan Turner <evturn@gmail.com>
2017-07-17 22:03:15 -0400

    Rewrite the exponent operator as recursive function

--- 06-recursive-functions.hs
+++ 06-recursive-functions.hs
@@ -9,3 +9,8 @@ factorial n | n > 0 = n * factorial (n - 1)
 sumdown :: Int -> Int
 sumdown 0         = 0
 sumdown n | n > 0 = n + sumdown (n - 1)
+
+-- 3.
+(^) :: Int -> Int -> Int
+(^) 0 = 1
+(^) m ^ n = m + (exponent m ^ (n - 1))
