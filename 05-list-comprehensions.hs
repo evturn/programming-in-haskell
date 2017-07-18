cd40453cd1030c1bb528bf807e3f167d0005159d
Evan Turner <evturn@gmail.com>
2017-07-17 21:18:41 -0400

    Add exercises 3 and 4 for chapter 5

--- 05-list-comprehensions.hs
+++ 05-list-comprehensions.hs
@@ -8,5 +8,9 @@ grid :: Int -> Int -> [(Int, Int)]
 grid m n = [(x, y) | x <- [0..m], y <- [0..n]]
 
 -- 3.
+square' :: Int -> [(Int, Int)]
+square' n = [(x, y) | (x, y) <- grid n n, x /= y]
+
+-- 4.
 replicate' :: Int -> a -> [a]
 replicate' n x = [x | _ <- [0..n]]
---
+++ 06-recursive-functions.hs
@@ -0,0 +1,4 @@
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 2) + fib (n - 1)
