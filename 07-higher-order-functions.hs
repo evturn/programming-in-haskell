d7faa8fedf6e6289082f3d730cc78e8848d87c08
Evan Turner <evturn@gmail.com>
2017-08-25 22:04:40 -0400

    Add first part of second chapter exercise of chapter 7

--- 07-higher-order-functions.hs
+++ 07-higher-order-functions.hs
@@ -1,4 +1,13 @@
+import Prelude hiding (all)
+-- 1.
 -- Show how the list comprehension [f x | x <- xs, p x] can be re-expressed using the higher-order functions map and filter.
 
 f1 :: (a -> Bool) -> (a -> b) -> [a] -> [b]
 f1 p f xs = map f $ filter p xs
+
+-- 2.
+-- Without looking at the definitions from the standard prelude, define the following higher-order library functions on lists.
+-- a.
+-- Decide if all elements of a list satisfy a predicate:
+all :: (Int -> Bool) -> [Int] -> Bool
+all f = foldr (\x y -> f x && y) True 
