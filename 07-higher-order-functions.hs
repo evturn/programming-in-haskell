99c74354d9f8ed86135bd50d673d4e3b11e7f695
Evan Turner <evturn@gmail.com>
2017-08-26 16:17:11 -0400

    Rewrite iterate library function

--- 07-higher-order-functions.hs
+++ 07-higher-order-functions.hs
@@ -1,4 +1,4 @@
-import Prelude hiding (all, any, takeWhile, dropWhile, map, filter)
+import Prelude hiding (all, any, takeWhile, dropWhile, map, filter, iterate)
 -- 1.
 -- Show how the list comprehension [f x | x <- xs, p x] can be re-expressed using the higher-order functions map and filter.
 
@@ -53,3 +53,15 @@ currry f = \x y -> f (x, y)
 
 uncurrry :: (a -> b -> c) -> ((a, b) -> c)
 uncurrry f = \(x, y) -> f x y
+
+-- 6.
+-- Redefine the functions chop8, map f, and iterate f using unfold:
+-- unfold p h t x 
+--  | p x       = []
+--  | otherwise = h x : unfold p h t (t x)
+chop8 :: [Int] -> [[Int]] 
+chop8 [] = []
+chop8 xs = take 8 xs : chop8 (drop 8 xs)
+
+iterate :: (a -> a) -> a -> [a]
+iterate f n = n : iterate f (f n) 
