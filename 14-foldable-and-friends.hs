8e8b84c88a8c3528a795e25e47c6b5cb9fd15250
Evan Turner <evturn@gmail.com>
2017-09-03 23:41:59 -0400

    Add exercises 2 and 3 for chapter 14

--- 14-foldable-and-friends.hs
+++ 14-foldable-and-friends.hs
@@ -4,3 +4,13 @@
 instance (Monoid a, Monoid b) => Monoid (a, b) where
   mempty = (mempty, mempty)
   (x1, y1) <> (x2, y2) = (x1 <> x2, y1 <> y2)
+
+-- 2.
+instance Monoid b => Monoid (a -> b) where
+  mempty = \_ -> mempty
+  f <> g = \x -> f x <> g x 
+
+-- 3.
+instance Traversable Maybe where
+  foldr _ _ Nothing = Nothing
+  foldr f x (Just y) = Just (f (x `mappend` y))
