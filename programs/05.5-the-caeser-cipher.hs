58092caeb6122b79c2f06cf3cd83e1db0061fc2a
Evan Turner <evturn@gmail.com>
2017-10-07 22:23:22 -0700

    Add frequency table to cipher exercise

--- programs/05.5-the-caeser-cipher.hs
+++ programs/05.5-the-caeser-cipher.hs
@@ -13,3 +13,20 @@ shift n c
 
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
+
+
+-- a Frequency table by analyzing a large volume of such text, one can derive the
+-- following percentage frequencies of the twenty-six letters of the alphabet.
+table :: [Float]
+table = [8.1, 1.5, 2.8, 4.5, 12.7, 2.2, 2.0, 6.1, 7.0, 
+         0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
+         6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
+
+lowers :: String -> Int
+lowers xs = length [x | x <- xs, x >= 'a' && x <= 'z']
+
+count :: Char -> String -> Int
+count x xs = length [x' | x' <- xs, x == x']
