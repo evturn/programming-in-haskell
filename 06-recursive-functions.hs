972c30330709d2841706bf943b81f4fea7c3540a
Evan Turner <evturn@gmail.com>
2017-10-09 22:23:22 -0700

    Update chapter exercises on recursion

--- 06-recursive-functions.hs
+++ 06-recursive-functions.hs
@@ -11,6 +11,17 @@ sumdown 0         = 0
 sumdown n | n > 0 = n + sumdown (n - 1)
 
 -- 3.
-(^) :: Int -> Int -> Int
-(^) 0 = 1
-(^) m ^ n = m + (m ^ (n - 1))
+-- Define the exponentiation operator `^` for non-negative integers using the same
+-- pattern of recursion as the multiplication operator `*`.
+(<^>) :: Int -> Int -> Int
+0 <^> _ = 0
+n <^> 0 = 1
+n <^> 1 = n 
+n <^> m = n * (n <^> (m - 1)) 
+
+-- 4.
+-- Euclid's algorithm is used for calculating the greatest common divisor of two
+-- non-negative integers
+euclid :: Int -> Int -> Int
+euclid n 0 = n
+euclid n m = euclid m (n `mod` m) 
