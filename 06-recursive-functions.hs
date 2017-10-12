a2e694fb8bc586bcc48f275ca114d8080a00e94a
Evan Turner <evturn@gmail.com>
2017-10-11 22:23:22 -0700

    Add chapter exercise explicitly showing recursivesteps

--- 06-recursive-functions.hs
+++ 06-recursive-functions.hs
@@ -31,3 +31,33 @@ n <^> m = n * (n <^> (m - 1))
 euclid :: Int -> Int -> Int
 euclid n 0 = n
 euclid n m = euclid m (n `mod` m) 
+
+-- 5.
+-- Show how `length [1,2,3]`, `drop 3 [1,2,3,4,5]`, and `init [1,2,3]` are evaluated
+length' :: [a] -> Int
+length' []     = 0
+length' (_:xs) = 1 + length' xs
+-- length' [1,2,3]
+-- 1 + (length' [2,3])
+-- 1 + 1 + (length' [3])
+-- 1 + 1 + 1 + (length' [])
+-- 1 + 1 + 1 + 0
+
+drop' :: Int -> [a] -> [a]
+drop' 0 xs     = xs
+drop' _ []     = []
+drop' n (_:xs) = drop' (n - 1) xs
+-- drop' 3 [1,2,3,4.5]
+-- drop' 2 [2,3,4,5]
+-- drop' 1 [3,4,5]
+-- drop' 0 [4,5]
+-- [4,5]
+
+init' :: [a] -> [a]
+init' [_]    = []
+init' (x:xs) = x : init' xs
+-- init' [1,2,3]
+-- 1 : init' [2,3]
+-- 1 : 2 : init' [3]
+-- 1 : 2 : []
+-- [1,2]
