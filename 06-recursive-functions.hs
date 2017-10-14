cd38d550c0709e14edf19f937c5e2e3b807ab912
Evan Turner <evturn@gmail.com>
2017-10-13 22:23:22 -0700

    Create recursive merge and sort of two lists

--- 06-recursive-functions.hs
+++ 06-recursive-functions.hs
@@ -94,3 +94,12 @@ elem' a (x:xs) =
   if a == x
      then True
   else elem' a xs
+
+-- 7.
+-- Define a recursive function hat merges two lists to give a single sorted list.
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) 
+  | x < y     = x : merge xs (y : ys)
+  | otherwise = y : merge (x : xs) ys
