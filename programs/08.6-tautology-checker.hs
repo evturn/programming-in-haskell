0b283cbd3f9b00c1bcb0d77fbbfcb5b4725c1856
Evan Turner <evturn@gmail.com>
2017-10-17 22:23:22 -0700

    Add comments on each pattern match in tautology checker

--- programs/08.6-tautology-checker.hs
+++ programs/08.6-tautology-checker.hs
@@ -6,21 +6,44 @@ data Prop = Const Bool
 
 type Assoc k v = [(k, v)]
 
+type Subst = Assoc Char Bool
+
 find :: Eq k => k -> Assoc k v -> v
 find k t = head [v | (k', v) <- t, k == k']
 
-type Subst = Assoc Char Bool
-
+-- | A function that evaluates a proposition given a substitution for its variables
+-- defined by pattern matching on the five possible forms that the proposition can
+-- have.
 eval :: Subst -> Prop -> Bool
+-- | The value of a constant proposition is simply the constant itself.
 eval _ (Const b)   = b
+-- | The value of a variable is obtained by looking up its value in the 
+-- substitution.
 eval s (Var x)     = find x s
+-- | The value of a conjunction is given by taking the conjunction of the values of
+-- the two argument propositions.
 eval s (Not p)     = not (eval s p)
 eval s (And p q)   = eval s p && eval s q
+-- | The value of an implication is obtained by the `<=` ordering on logical values.
 eval s (Imply p q) = eval s p <= eval s q
 
+-- | A function that returns a list of all the variables in a proposition.
 vars :: Prop -> [Char]
 vars (Const _)   = []
 vars (Var x)     = [x]
 vars (Not p)     = vars p
 vars (And p q)   = vars p ++ vars q
 vars (Imply p q) = vars p ++ vars q
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply
+        (Var 'A') (Var 'B'))) (Var 'B')
