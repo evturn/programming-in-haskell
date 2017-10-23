ed5dcfb7e057f613aef4f8043f5b504757f72b71
Evan Turner <evturn@gmail.com>
2017-10-23 05:20:07 -0400

    Complete exercise 6 from chapter 8

--- 08-declaring-types-and-classes.hs
+++ 08-declaring-types-and-classes.hs
@@ -83,3 +83,12 @@ folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
 folde f g (Val x)   = f x
 folde f g (Add x y) = g (folde f g x) (folde f g y)
 
+-- 6.
+-- Using `folde` define a function `eval` that evaluates an
+-- expression to an integer value, and a function `size`
+-- that calculates the number of values in an expression.
+eval :: Expr -> Int 
+eval = folde (+0) (+)
+
+size :: Expr -> Int
+size = folde (\_ -> 1) (+)
