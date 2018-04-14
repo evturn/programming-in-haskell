5da0f8b4004fed72ce9380fc239966d06d87b05c
Evan Turner <ev@evturn.com>
2018-04-14 04:12:05 -0400

    Fix function in chapter 5 exercises, closes #1

--- 05-list-comprehensions/05.7-exercises.hs
+++ 05-list-comprehensions/05.7-exercises.hs
@@ -27,7 +27,7 @@ factors :: Int -> [Int]
 factors n = [x | x <- [1..n], n `mod` x == 0]
 
 perfects :: Int -> [Int]
-perfects n = [sum (factors x) | x <- [1..n]]
+perfects n = [x | x <- [1..n], sum (factors x) - x == x]
 
 -- 7.
 -- Show how the list comprehension [(x, y) | x <- [1, 2], y <- [3, 4]] with two generators can be
